c0d6ee48648e8a56c130166c7778913e42a7042d
Onah-NeLson <Onahnelson2@gmail.com>
2022-10-14 02:44:42 -0700

    Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * man - Entry point for the program.
+ *
+ * Return: 0 if no errors, return non zeron if errors
+ */
+int main(void)
+{
+	printf("With proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
