1e1f9b97754e51d1ea0fb097c2d9c983138c176e
Onah-NeLson <Onahnelson2@gmail.com>
2022-10-17 05:48:06 -0700

    Task5-Hello-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
- **main - Entry point
+ * main - print a c program using printf
  *
- **Return: return 1 after program exec
- **/
-
+ * Return: 0
+ */
 int main(void)
 {
-	printf("with proper grammer ,
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
