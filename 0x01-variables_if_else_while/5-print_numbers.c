20493f50f829e7c38b2f18c0e225c35706851116
Onah-NeLson <Onahnelson2@gmail.com>
2022-10-18 09:44:59 -0700

    Task5print-numb.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,11 +7,11 @@
 int main(void)
 {
 	char i;
-	
-	for (i= '0'; i <= '9'; i++)
+
+	for (i = '0'; i <= '9'; i++)
 {
 	putchar(i);
-} 
-        putchar('\n');
-	return(0);
+}
+	putchar('\n');
+	return (0);
 }
