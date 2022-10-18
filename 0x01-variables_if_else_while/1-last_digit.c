c4f4409a1662c55043bd04428407c8bf61b706dd
Onah-NeLson <Onahnelson2@gmail.com>
2022-10-18 09:25:02 -0700

    Task4-print-aphabt.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,5 +1,4 @@
 #include <time.h>
-#include <stdio.h>
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
@@ -14,7 +13,7 @@ int main(void)
 
 	int m;
 
-	srand(time(0));
+	rand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	m = n % 10;
