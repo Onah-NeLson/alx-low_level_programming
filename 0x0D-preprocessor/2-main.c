178609d6c646af15364174aa88d2faf5f443c35d
Onah-NeLson <Onahnelson2@gmail.com>
2022-11-14 21:08:11 -0800

    committed

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - prints the name of the file.
+*main - prints the name of
+*the file it was compiled from
 *
-* Return: Always 0.
+*Return: nothing to be returned
 */
+
 int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
-
-
