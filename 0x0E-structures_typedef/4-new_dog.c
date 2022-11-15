402ba5bbea905d3617b72f572261e1dd0baa6945
Onah-NeLson <Onahnelson2@gmail.com>
2022-11-14 22:59:37 -0800

    commit

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -23,9 +23,10 @@ dog_t *new_dog(char *name, float age, char *owner)
 	return (NULL);
 	}
 
-	for (lname = 0; name[lname]; lname++);
-
-	for (lowner = 0; owner[lowner]; lowner++);
+	for (lname = 0; name[lname]; lname++)
+		;
+	for (lowner = 0; owner[lowner]; lowner++)
+		;
 	p_dog->name = malloc(lname + 1);
 	p_dog->owner = malloc(lowner + 1);
 
